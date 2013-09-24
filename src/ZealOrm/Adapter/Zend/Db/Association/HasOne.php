9095550b4ee3aae33e413f483d499fb71e0fe137
Tim Fountain <tim@tfountain.co.uk>
2013-09-24 10:25:22 -0400

    Added has one association

---
+++ src/ZealOrm/Adapter/Zend/Db/Association/HasOne.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ZealOrm\Adapter\Zend\Db\Association;
+
+use ZealOrm\Adapter\Zend\Db\Association\AbstractAssociation;
+
+class HasOne extends AbstractAssociation
+{
+    public function getForeignKey()
+    {
+        // TODO tidy this up with a better default
+        $adapterOptions = $this->getTargetMapper()->getAdapterOptions();
+        return $adapterOptions['primaryKey'];
+    }
+
+    public function buildQuery()
+    {
+        $query = $this->getTargetMapper()->buildQuery();
+
+        $foreignKey = $this->getForeignKey();
+
+        $query->where(array($foreignKey => $this->getColumnValue($this->getSource(), $foreignKey)));
+
+        // $sql = new \Zend\Db\Sql\Sql($this->getTargetMapper()->getAdapter()->getDb());
+        // echo $sql->getSqlStringForSqlObject($query);
+        // exit;
+
+        return $query;
+    }
+
+    public function loadData()
+    {
+        $query = $this->buildQuery();
+
+        return $this->getTargetMapper()->fetchObject($query);
+    }
+}
