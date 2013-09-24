d9d01e6e0c0ee3e1818af748ef74653c833d53ee
Tim Fountain <tim@tfountain.co.uk>
2013-09-24 10:29:46 -0400

    Added has one association class

---
+++ src/ZealOrm/Model/Association/HasOne.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ZealOrm\Model\Association;
+
+use ZealOrm\Model\Association\AbstractAssociation;
+
+class HasOne extends AbstractAssociation
+{
+    public function buildQuery()
+    {
+        $query = $this->getTargetMapper()->buildQuery();
+
+        $foreignKey = $this->getForeignKey();
+
+        $query->where(array($foreignKey => $this->getColumnValue($this->getSource(), $foreignKey)));
+
+        return $query;
+    }
+
+    public function loadData()
+    {
+        $query = $this->buildQuery();
+
+        return $this->getTargetMapper()->fetchObject($query);
+    }
+
+    /**
+     * Returns the foreign key column name
+     *
+     * @return string
+     */
+    public function getForeignKey()
+    {
+        return $this->getOption('foreignKey');
+    }
+}
