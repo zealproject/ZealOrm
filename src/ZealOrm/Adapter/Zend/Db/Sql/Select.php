78c43922ee6426e4e8c6b51c0134ce89e3f9c543
Tim Fountain <tim@tfountain.co.uk>
2013-09-26 12:09:52 -0400

    Tweaking query setup for zend db adapter

--- src/ZealOrm/Adapter/Zend/Db/Sql/Select.php
+++ src/ZealOrm/Adapter/Zend/Db/Sql/Select.php
@@ -3,8 +3,9 @@
 namespace ZealOrm\Mapper\Adapter\Zend\Db\Sql;
 
 use Zend\Db\Sql\Select as ZendSelect;
+use ZealOrm\Adapter\Query\QueryInterface;
 
-class Select extends ZendSelect
+class Select extends ZendSelect implements QueryInterface
 {
 
 }
--- src/ZealOrm/Adapter/Zend/Db/Sql/Sql.php
+++ src/ZealOrm/Adapter/Zend/Db/Sql/Sql.php
@@ -5,7 +5,17 @@ namespace ZealOrm\Adapter\Zend\Db\Sql;
 use Zend\Db\Sql\Sql as ZendSql;
 use ZealOrm\Adapter\Query\QueryInterface;
 
-class Sql extends ZendSql implements QueryInterface
+class Sql extends ZendSql
 {
+    public function select($table = null)
+    {
+        if ($this->table !== null && $table !== null) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'This Sql object is intended to work with only the table "%s" provided at construction time.',
+                $this->table
+            ));
+        }
 
+        return new Select(($table) ?: $this->table);
+    }
 }
